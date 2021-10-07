e91bfe446668b42a537124176e4358bf1d9ab71c
alamin500 <nalamin500@gmail.com>
2021-10-07 10:11:40 +0600

    h

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
 import "./App.css";
 import initailizeAuthentication from "./firebase.initialize";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signOut,
+} from "firebase/auth";
 import { useState } from "react";
 
-const provider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider();
+const githunProvider = new GithubAuthProvider();
 initailizeAuthentication();
 
 function App() {
   const [user, setUser] = useState({});
+  const auth = getAuth();
   const handleGoogleSignIn = () => {
-    const auth = getAuth();
-    signInWithPopup(auth, provider).then((result) => {
-      const { displayName, email, photoURL } = result.user;
-      const loggedInUser = {
-        name: displayName,
-        email: email,
-        photo: photoURL,
-      };
-      setUser(loggedInUser);
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL,
+        };
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githunProvider).then((result) => {
+      const user = result.user;
+      console.log(user);
+    });
+  };
+  const handleSignOUt = () => {
+    signOut(auth).then(() => {
+      setUser({});
     });
   };
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <button onClick={handleGoogleSignIn}> Google Sign In</button>
+      <button onClick={handleGithubSignIn}>Github Sign In</button>
+      <button onClick={handleSignOUt}></button>
       <br />
       {user.email && (
         <div>
-          <h2>Welcome{user.name}</h2>
+          <h2>Welcome {user.name}</h2>
+          <p>I know your email: {user.email}</p>
+          <img src={user.photo} alt="" />
         </div>
       )}
     </div>
